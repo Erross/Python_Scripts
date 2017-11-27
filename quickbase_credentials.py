362ef1b9c5851b8ebb8939506c4403c5a0d83af0
EWAN ROSS <erross@monsanto.com>
2017-11-27 13:11:11 -0600

    First Commit

---
+++ quickbase_credentials.py
@@ -0,0 +1,15 @@
+#Script to get credentials from quickbase database with userid and password
+
+import requests
+from xml.etree import ElementTree as ET
+
+def get_creds():
+    r = requests.get('https://seedbin.quickbase.com/db/main?a=API_Authenticate&username='+username+'&password='+password+'Mw280381&hours=24')
+
+    tree = ET.fromstring(r.content)
+
+    for child in tree.findall('ticket'):
+         Ticket = child.text
+    return Ticket
+
+print(get_creds())
